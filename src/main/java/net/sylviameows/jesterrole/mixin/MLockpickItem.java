b4dda863da7a476315eebc787b08a8023168639d
sylviameows <me@sylviameo.ws>
2025-11-25 23:37:21 -0600

    fix lockpick crash

--- src/main/java/net/sylviameows/jesterrole/mixin/MLockpickItem.java
+++ src/main/java/net/sylviameows/jesterrole/mixin/MLockpickItem.java
@@ -1,9 +1,13 @@
 package net.sylviameows.jesterrole.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.doctor4t.trainmurdermystery.TMM;
+import dev.doctor4t.trainmurdermystery.cca.GameWorldComponent;
 import dev.doctor4t.trainmurdermystery.client.TMMClient;
 import dev.doctor4t.trainmurdermystery.item.LockpickItem;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import net.sylviameows.jesterrole.Jester;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,9 +20,10 @@ public class MLockpickItem {
             method = "useOnBlock",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isSneaking()Z")
     )
-    private static boolean jester$useOnBlock(PlayerEntity player) {
+    private static boolean jester$useOnBlock(PlayerEntity player, @Local(name = "world") World world) {
         if (player.isSneaking()) {
-            if (TMMClient.gameComponent.isRole(player, Jester.ROLE)) {
+            GameWorldComponent game = GameWorldComponent.KEY.get(world);
+            if (game.isRole(player, Jester.ROLE)) {
                 player.sendMessage(Text.translatable("tip.lockpick.killer_only"), true);
                 return false;
             }
